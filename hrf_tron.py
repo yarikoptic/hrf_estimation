b6d20c4ebfb52d422b6d7711627c054fd7946950
Fabian Pedregosa <fabian@fseoane.net>
2013-05-31 11:30:31 +0200

    wip

--- hrf_tron.py
+++ hrf_tron.py
@@ -68,14 +68,14 @@ def f(w, X_, Y_, Z_, size_u, alpha, u0):
     u, v, c = W[:size_u], W[size_u:size_u + size_v], W[size_u + size_v:]
     return obj(X_, Y_, Z_, u, v, c, alpha, u0)
 
-def fprime(w, X_, Y_, Z_, size_u, alpha, u0):
+def fprime(x0, X_, Y_, Z_, size_u, alpha, u0):
     X_ = splinalg.aslinearoperator(X_)
     Z_ = splinalg.aslinearoperator(Z_)
     n_task = Y_.shape[1]
     size_v = X1.shape[1] / size_u
-    W = w.reshape((-1, n_task), order='F')
-    u, v, c = W[:size_u], W[size_u:size_u + size_v], W[size_u + size_v:]
-    tmp = Y_ - matmat2(X_, u, v, n_task) - Z_.matmat(c)
+    W = x0.reshape((-1, n_task), order='F')
+    u, v, w = W[:size_u], W[size_u:size_u + size_v], W[size_u + size_v:]
+    tmp = Y_ - matmat2(X_, u, v, n_task) - Z_.matmat(w)
     grad = np.empty((size_u + size_v + Z_.shape[1], n_task))  # TODO: do outside
     grad[:size_u] = rmatmat1(X_, v, tmp, n_task) - alpha * (u - u0[:, None])
     grad[size_u:size_u + size_v] = rmatmat2(X_, u, tmp, n_task)
@@ -83,19 +83,22 @@ def fprime(w, X_, Y_, Z_, size_u, alpha, u0):
     return - grad.reshape((-1,), order='F')
 
 
-def hess(s, w, X_, Y_, Z_, size_u, alpha, u0):
+def hess(s, x0, X_, Y_, Z_, size_u, alpha, u0):
     # TODO: regularization
     X_ = splinalg.aslinearoperator(X_)
     Z_ = splinalg.aslinearoperator(Z_)
     n_task = Y_.shape[1]
     size_v = X1.shape[1] / size_u
-    W = w.reshape((-1, n_task), order='F')
+    W = x0.reshape((-1, n_task), order='F')
+    S = s.reshape((-1, n_task), order='F')
     XY = X_.rmatvec(Y_)
-    u, v, c = W[:size_u], W[size_u:size_u + size_v], W[size_u + size_v:]
-    s1, s2, s3 = s[:size_u], s[size_u:size_u + size_v], s[size_u + size_v:]
+    u, v, w = W[:size_u], W[size_u:size_u + size_v], W[size_u + size_v:]
+    s1, s2, s3 = S[:size_u], S[size_u:size_u + size_v], S[size_u + size_v:]
     W2 = X_.rmatvec(matmat2(X_, u, v, n_task))
-    W2 = W2.reshape((-1, s1.shape[0]), order='F')
-    XY = XY.reshape((-1, s1.shape[0]), order='F')
+    W2 = W2.reshape((n_task, size_v, size_u), order='C')
+    XY = XY.reshape((n_task, size_v, size_u), order='C')
+    C = X_.rmatvec(Z_.matvec(w))
+    C = C.reshape((n_task, size_v, size_u), order='C')
 
     tmp = matmat2(X_, s1, v, n_task)
     As1 = rmatmat1(X_, v, tmp, n_task)
@@ -108,10 +111,11 @@ def hess(s, w, X_, Y_, Z_, size_u, alpha, u0):
     Cts1 = Z_.rmatvec(tmp.T)
 
     tmp = matmat2(X_, u, s2, n_task)
-    Bs2 = rmatmat1(X_, v, tmp, n_task) + W2.T.dot(s2) - XY.T.dot(s2)
+    Bs2 = rmatmat1(X_, v, tmp, n_task) + W2.T.dot(s2) - XY.T.dot(s2) + \
+        C.T.dot(s2)
 
     tmp = matmat2(X_, s1, v, n_task)
-    Bts1 = rmatmat2(X_, u, tmp, n_task) + W2.dot(s1) - XY.dot(s1)
+    Bts1 = rmatmat2(X_, u, tmp, n_task) + W2.dot(s1) - XY.dot(s1) + C.dot(s1)
 
     tmp = Z_.matvec(s3)
     Es3 = rmatmat2(X_, u, tmp, n_task)
@@ -128,7 +132,7 @@ def hess(s, w, X_, Y_, Z_, size_u, alpha, u0):
     return np.concatenate((line0, line1, line2))
 
 if __name__ == '__main__':
-    n_target = 1
+    n_target = 2
     np.random.seed(0)
     X1 = np.random.randn(12, 10)
     Z1 = np.random.randn(12, 10)
@@ -136,18 +140,20 @@ if __name__ == '__main__':
     size_u = 5
     size_v = 2
     canonical = np.random.randn(size_u)
-    x0 = np.random.randn(size_u + size_v + Z1.shape[1], Y1.shape[1]).ravel()
+    x0 = np.random.randn(size_u + size_v + Z1.shape[1],
+                         Y1.shape[1]).ravel()
     print(optimize.check_grad(f, fprime, x0, X1, Y1, Z1, size_u, 1.,
                               canonical))
 
     import numdifftools as nd
     import pylab as pl
     H = nd.Hessian(lambda x: f(x, X1, Y1, Z1, size_u, 0., canonical))
-    pl.matshow(H(x0)[:size_u+size_v, :size_u+size_v])
+    pl.matshow(H(x0))
     pl.title('numdifftools')
     pl.colorbar()
+    pl.show()
 
-    E = np.eye(size_u + size_v + Z1.shape[1])
+    E = np.eye(n_target * (size_u + size_v + Z1.shape[1]))
     out = []
     for i in range(E.shape[0]):
         ei = E[i]
@@ -155,6 +161,6 @@ if __name__ == '__main__':
         tmp = hess(ei, x0, X1, Y1, Z1, size_u, 0., canonical)
         out.append(tmp.ravel())
     true_H = np.array(out)
-    pl.matshow(true_H[:size_u+size_v, :size_u+size_v])
+    pl.matshow(true_H)
     pl.colorbar()
     pl.show()
